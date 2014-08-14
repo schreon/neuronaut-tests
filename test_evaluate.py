aebe3b10479f925643c7d03b65b9d0981cfb7ac1
schreon <schreon.loeder@gmail.com>
2014-08-15 00:40:08 +0200

    removed old tests. more tutorial-like comments

--- test_evaluate.py
+++ test_evaluate.py
@@ -22,7 +22,6 @@ from neuro.sarprop import SARPROP
 from neuro.stopping import EarlyStopping
 from neuro.training import FullBatchTrainer, SGDTrainer
 from neuro.weightdecay import Renormalize
-from neuro import OpenClContext
 
 
 logging.basicConfig(level=logging.INFO)
@@ -32,62 +31,83 @@ class Test(unittest.TestCase):
 
 
     def testEvaluate(self):
-        ctx = neuro.create("MyContext", CUDAContext)(num_workers=4)    
+        # First, choose whether CUDA or OpenCL should be used as backend.
+        # In this example we stick to the CUDAContext. But you can just
+        # swap that with OpenCLContext.
+        ctx = neuro.create("MyContext", CUDAContext)()
+        
+        # In the test package, a small regression dataset is prepared.
+        # The task is to predict the age of abalone based on 8 attributes.
         inp, targ, minimum, maximum = abalone.get_patterns()
-        
-        #inp = numpy.random.uniform(0.0, 1.0, (50*1024, 1024)).astype(numpy.float32)
-        #targ = numpy.random.uniform(0.0, 1.0, (50*1024, 48)).astype(numpy.float32)
-        
-        # test/train ratio 50% : 50%
+
+        # You should use a portion of the data for validating
+        # the generalization performance of the network.
+        # Simply split your numpy arrays along the first dimension:
         n = int(0.5*inp.shape[0])
         inp_test = inp[:n]
         targ_test = targ[:n]        
         inp = inp[n:]
-        targ = targ[n:]
-        
+        targ = targ[n:]        
         data_train = (inp, targ)
         data_test = (inp_test, targ_test)
         
-        MyNetwork = neuro.create("MyNetwork",
+        # Now we need to create the network class. neuronaut comes
+        # with a factory method which takes a series of Mixin classes
+        # and combines them. The first parameter of the create-method
+        # is the name you wish for your new class:
+        NetworkClass = neuro.create("MyNetwork",
                                  FeedForwardNeuralNetwork,
                                  Regression,
                                  NaNMask,
                                  )
-        network_args = dict(context=ctx, input_shape=inp.shape[1])
-                 
-        MyTrainer = neuro.create("MyTrainer", 
+
+        # Next, we must specify the structure of the neural network.
+        # Here, we use 2 hidden layers with the logistic activation function
+        # and an output layer with linear units.
+        network_structure = [
+           dict(num_units=inp.shape[1]),
+           dict(num_units=64, function=ctx.logistic, derivative=ctx.logistic_derivative),
+           dict(num_units=64, function=ctx.logistic, derivative=ctx.logistic_derivative),
+           dict(num_units=targ.shape[1], function=ctx.linear, derivative=ctx.linear_derivative)
+        ]
+
+        # To train the network, we also need a Trainer class.
+        # In this case we want to do full batch training with
+        # the learning algorithm RPROP. To determine when to
+        # stop training, we use early stopping. Since the
+        # abalone dataset is very small and noisy, we need a
+        # regularizer to avoid overfitting. We simply add the
+        # Renormalize mixin, which is a special kind of weight
+        # decay which constraints the length of the weight vector
+        # of each neuron.
+        TrainerClass = neuro.create("MyTrainer", 
                                  FullBatchTrainer, 
                                  Backpropagation, 
                                  EarlyStopping, 
                                  RPROP, 
                                  Renormalize
                                  )
-        trainer_args = dict(min_steps=1000,
+        
+        
+        trainer_options = dict(min_steps=1000,
                             validation_frequency=5, # validate the model every 5 steps
                             validate_train=True, # also validate on training set
                             logging_frequency=2.0, # log progress every 2 seconds
                             max_weight_size=15.0,
                             max_step_size=0.01
                             )
-        
-        network_structure = [
-           dict(num_units=64, function=ctx.softplus, derivative=ctx.softplus_derivative),
-           dict(num_units=64, function=ctx.softplus, derivative=ctx.softplus_derivative),
-           dict(num_units=targ.shape[1], function=ctx.linear, derivative=ctx.linear_derivative)
-        ]
-        
+
         evaluator = ConfigurationEvaluator(
           data_train=data_train,
           data_test=data_test,
-          NetworkClass=MyNetwork,
-          TrainerClass=MyTrainer,
-          network_args=network_args,
-          trainer_args=trainer_args,
+          NetworkClass=NetworkClass,
+          TrainerClass=TrainerClass,
+          network_args=dict(context=ctx),
+          trainer_args=trainer_options,
           network_structure=network_structure
           )
 
-        evaluator.evaluate(num_evaluations=1, file_name="data/Abalone-Test.pik")
-
+        evaluator.evaluate(num_evaluations=5, file_name="data/Abalone-Test.pik")
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testEvaluate']
